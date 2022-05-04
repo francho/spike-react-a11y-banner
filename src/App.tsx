ee839936bc472d2691536e51566368eebdad3edd
Francho Joven <lab@francho.org>
2022-05-05 00:02:32 +0200

    chore: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import './App.css'
 
 import React from 'react'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 
 import ReactSlickBanner from './components/ReactSlickBanner'
 
 function App() {
   return (
-    <div className="App">
-      <div className="banners">
-        <ReactSlickBanner />
+    <Router>
+      <div className="App">
+        <h1>spike react-a11y-banner</h1>
+        <div className="banners">
+          <Routes>
+            <Route path="/" element={<div>home</div>} />
+            <Route path="/react-slick" element={<ReactSlickBanner />} />
+          </Routes>
+        </div>
+        <footer>
+          <ul>
+            <li>
+              <Link to="/">home</Link>
+            </li>
+            <li>
+              <Link to="/react-slick">react-slick</Link>
+            </li>
+          </ul>
+        </footer>
       </div>
-    </div>
+    </Router>
   )
 }
 
